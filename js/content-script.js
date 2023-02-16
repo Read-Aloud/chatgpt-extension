f5cfa5c741f276d47d43c1c0c93b5c3bb0c7fee0
Hai Phan <hai.phan@gmail.com>
2023-02-15 16:25:26 -0800

    fourth

--- js/content-script.js
+++ js/content-script.js
@@ -7,19 +7,31 @@ const mainObservable = rxjs.fromMutationObserver(document.getElementById("__next
     rxjs.startWith(document),
     rxjs.concatMap(el => rxjs.from(el.getElementsByTagName("main")).pipe(rxjs.take(1))),
     rxjs.distinctUntilChanged(),
+    rxjs.debounceTime(3*1000),
     rxjs.tap(main => console.debug("Main changed", main))
   )
 
-const newestParagraphObservable = mainObservable
+const paraObservable = mainObservable
   .pipe(
-    rxjs.debounceTime(3*1000),
     rxjs.switchMap(main => rxjs.fromMutationObserver(main, {subtree: true, childList: true})),
     rxjs.concatMap(records => rxjs.from(records)),
     rxjs.concatMap(record => rxjs.from(record.addedNodes)),
     rxjs.filter(node => node.nodeType == 1),
-    rxjs.concatMap(el => rxjs.from(el.getElementsByTagName("p")).pipe(rxjs.takeLast(1))),
-    rxjs.distinctUntilChanged(),
-    rxjs.tap(para => console.debug("Newest paragraph", para))
+    rxjs.concatMap(el => el.tagName == "P" ? rxjs.of(el) : rxjs.from(el.getElementsByTagName("p"))),
+    rxjs.tap(para => console.debug("Para changed", para))
   )
 
-newestParagraphObservable.subscribe()
+const textObservable = paraObservable
+  .pipe(
+    rxjs.switchMap(para => {
+      return rxjs.fromMutationObserver(para, {subtree: true, characterData: true})
+        .pipe(
+          rxjs.map(() => para.textContent),
+          rxjs.startWith(para.textContent)
+        )
+    }),
+    rxjs.distinctUntilChanged(),
+    rxjs.tap(text => console.log("Text changed", text))
+  )
+
+textObservable.subscribe()
